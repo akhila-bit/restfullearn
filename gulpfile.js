9c072d9fa758bb05c84dc75217fddfa732c959f5
Sreenath Thalla <akhila@automate.io>
2024-01-31 00:25:58 +0530

    gult pere rende html2

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ const rename = require("gulp-rename");
 
 gulp.task("ejs", function () {
   return gulp
-    .src("./views/pages/*.ejs")
+    .src("./views/*.ejs")
     .pipe(ejs())
     .pipe(rename({ extname: ".html" }))
     .pipe(gulp.dest("./dist"));
